671e7350fc0ba67d8493e75a198e02bafbe1637d
Touhed <mr.touhed.i@outlook.com>
2024-06-04 19:40:56 +0600

    remove sencitive

--- app/controllers/products.controllers.js
+++ app/controllers/products.controllers.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const product_collection = require("../models/product.models")
 
 const get_products = async (req,res) =>{
@@ -8,6 +9,17 @@ const get_products = async (req,res) =>{
         console.log(error);
     }
 }
+
+const get_single_product = async (req,res) =>{
+    try {
+        const productId = req.params.id;
+        const product = await product_collection.findOne({_id: new ObjectId(productId)});
+        res.status(200).json({status:true,product})
+    } catch (error) {
+        
+    }
+}
+
 
 const add_product = async (req,res) =>{
     try {
@@ -20,4 +32,50 @@ const add_product = async (req,res) =>{
     }
 }
 
-module.exports = {get_products,add_product}
+
+
+const remove_product = async (req,res) =>{
+    try {
+        const productId = req.params.id;
+        const result = await product_collection.deleteOne({_id: new ObjectId(productId)});
+       
+        if(result.deletedCount > 0){
+            return res.status(202).json({status:true,result})
+        }else{
+            return res.status(400).json({status:false,result})
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const update_product = async (req,res) => {
+    try {
+        
+        const bodyData = req.body;
+        const productId = req.params.id;
+        const query = {_id: new ObjectId(productId)};
+        const updated = {$set:{
+            name:bodyData.name,
+            price:bodyData.price,
+            img:bodyData.img,
+            catagory:bodyData.catagory,
+            details:bodyData.details,
+            exclusive:bodyData.exclusive,
+            reguler_price:bodyData.reguler_price}
+        }
+
+        
+        const result = await product_collection.updateOne(query,updated,{});
+        
+        if(result.modifiedCount > 0){
+            return res.status(202).json({status:true,result})
+        }
+        return res.status(404).json({status:false,result})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {get_products,add_product,get_single_product,remove_product,update_product}
--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { get_products, add_product } = require('../controllers/products.controllers');
+const { get_products, add_product, get_single_product, remove_product, update_product } = require('../controllers/products.controllers');
 
 const ProductRouter = express.Router();
 
@@ -7,6 +7,9 @@ const ProductRouter = express.Router();
 
 ProductRouter.get("/products", get_products)
 ProductRouter.post("/product", add_product)
+ProductRouter.get("/product/:id", get_single_product)
+ProductRouter.delete("/product/:id", remove_product)
+ProductRouter.patch("/product/:id", update_product)
 
 
 
